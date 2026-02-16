b6222915ecd6f92e603039a2cda268dc18d200c9
Leroy H. Mason <https://github.com/LHMisme420>
2026-02-16 11:23:35 -0500

    Add simple on-chain anchor contract

---
+++ Verifier.sol
@@ -0,0 +1,10 @@
+﻿// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Verifier {
+    event ProofAnchored(bytes32 indexed root);
+
+    function anchor(bytes32 root) external {
+        emit ProofAnchored(root);
+    }
+}
